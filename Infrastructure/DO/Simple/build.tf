ffbe03d65400304b9ed8e8a61a09c941f02be19e
bneg <jeremy@sectiva.com>
2017-11-05 15:59:23 -0800

    Fixed paths in build, sleep timer

--- Infrastructure/DO/Simple/build.tf
+++ Infrastructure/DO/Simple/build.tf
@@ -1,6 +1,6 @@
 variable "do_token" {}
 variable "pub_key" {}
-variable "pvt_key" {
+variable "pvt_key" {}
 variable "ssh_fingerprint" {}
 
 provider "digitalocean" {
@@ -17,18 +17,17 @@ resource "digitalocean_droplet" "empire-server" {
        "${var.ssh_fingerprint}"
     ]
 
-connection {
-    user = "root"
-    type = "ssh"
-    private_key = "${file(var.pvt_key)}"
-    timeout = "2m"
-}
+    connection {
+        user = "root"
+        type = "ssh"
+        private_key = "${file(var.pvt_key)}"
+        timeout = "2m"
+      }
 
 provisioner "local-exec" {
-    command = "sleep -Seconds 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root --private-key '${ssh-keys},' -i '${digitalocean_droplet.empire-server.ipv4_address},' Empire.yml"
+    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root --private-key '${var.pvt_key},' -i '${digitalocean_droplet.empire-server.ipv4_address},' Empire.yml"
   }
 }
-
 
 output "Empire C2 Server IP" {
     value = "${digitalocean_droplet.empire-server.ipv4_address}"
